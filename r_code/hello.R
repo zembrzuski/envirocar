65c7c7380722fcb7b40af72b0993320dcbcf9687
Your Name <you@example.com>
2017-04-21 14:43:56 -0300

    doing simple analysis with r
    - just plotted duration historgram of tracks

---
+++ r_code/hello.R
@@ -0,0 +1,8 @@
+setwd("~/labs/envirocar/r_code")
+
+duration = read.csv("../data_integration/duration.csv")  # read csv file 
+duration_as_list = duration[,1]
+
+hist(duration_as_list)
+hist(duration_as_list[duration_as_list < 100])
+hist(duration_as_list[duration_as_list < 60])
