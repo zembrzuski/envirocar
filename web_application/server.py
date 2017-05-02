e28d78af74cea7080237a48ce24a71a6ac4209d4
Your Name <you@example.com>
2017-05-01 21:32:29 -0300

    solving cors using jquery + flask

--- web_application/server.py
+++ web_application/server.py
@@ -1,17 +1,32 @@
-#!/usr/bin/python
+from flask import Flask
+from flask import send_file
+import requests
+import json
+import numpy as np
 
-import SimpleHTTPServer
-import SocketServer
+app = Flask(__name__)
 
-PORT = 8000
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-Handler.extensions_map.update({
-    '.webapp': 'application/x-web-app-manifest+json',
-});
+@app.route('/')
+def send_js():
+    return send_file('teste.html')
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-print('servin')
-httpd.serve_forever()
+@app.route('/find_track/<track>')
+def find_track(track):
+    resp = requests.get('http://localhost:9200/envirocar/group/{}'.format(track))
+    loaded = json.loads(resp.content.decode('utf-8'))
 
+    features = loaded['_source']['features']
+
+    coords = list(map(lambda x: x['geometry']['coordinates'], features))
+    coordinates = list(map(lambda x: {'lng': x[0], 'lat': x[1]}, coords))
+
+    lat_center = np.mean(list(map(lambda x: x[1], coords)))
+    lng_center = np.mean(list(map(lambda x: x[0], coords)))
+
+    return "oi"
+
+
+if __name__ == "__main__":
+    app.run()
