1868bd3e5cec799b1bc462fe4d99ce66d4406db4
Your Name <you@example.com>
2017-05-06 16:59:51 -0300

    extracting create-dashboard-string to separate file
    just to keep the clean code

--- web_application/principal_js.js
+++ web_application/principal_js.js
@@ -18,59 +18,19 @@ function initMap() {
 
         vel_max = resp['vel']['max'];
 
-        // colocar tb o tipo de gasosa, para paoder comparar com o co2.
-
-        dashboard_string =
-            "velocidade:" +
-            "<ul>" +
-            "<li>maxima: " + resp['vel']['max'].toFixed(2) + " " + resp['vel']['unit'] + "</li>" +
-            "<li>media: "  + resp['vel']['med'].toFixed(2) + " " + resp['vel']['unit'] + "</li>" +
-            "<li>minima: " + resp['vel']['min'].toFixed(2) + " " + resp['vel']['unit'] + "</li>" +
-            "</ul> <br/>" +
-
-            "co2:" +
-            "<ul>" +
-            "<li>maxima: " + resp['co2']['max'].toFixed(2) + " " + resp['co2']['unit'] + "</li>" +
-            "<li>media: "  + resp['co2']['med'].toFixed(2) + " " + resp['co2']['unit'] + "</li>" +
-            "<li>minima: " + resp['co2']['min'].toFixed(2) + " " + resp['co2']['unit'] + "</li>" +
-            "</ul> <br/>" +
-
-            "rpm:" +
-            "<ul>" +
-            "<li>maxima: " + resp['rpm']['max'].toFixed(2) + " " + resp['rpm']['unit'] + "</li>" +
-            "<li>media: "  + resp['rpm']['med'].toFixed(2) + " " + resp['rpm']['unit'] + "</li>" +
-            "<li>minima: " + resp['rpm']['min'].toFixed(2) + " " + resp['rpm']['unit'] + "</li>" +
-            "</ul> <br/>" +
-
-            "engine load:" +
-            "<ul>" +
-            "<li>maxima: " + resp['engine-load']['max'].toFixed(2) + " " + resp['engine-load']['unit'] + "</li>" +
-            "<li>media: "  + resp['engine-load']['med'].toFixed(2) + " " + resp['engine-load']['unit'] + "</li>" +
-            "<li>minima: " + resp['engine-load']['min'].toFixed(2) + " " + resp['engine-load']['unit'] + "</li>" +
-            "</ul> <br/>" +
-
-            "deslocamento:" +
-            "<ul>" +
-            "<li>inicio: " + resp['tempo-inicio'] + "</li>" +
-            "<li>fim: " + resp['tempo-fim'] + "</li>" +
-            "<li>duracao: " + resp['duracao'] + "</li>" +
-            "<li>vm: " + resp['vm'].toFixed(2) + "km/h</li>" +
-            "<li>distancia total: " + resp['total-distance'].toFixed(2) + "km </li>" +
-            "<li>distancia linha reta: " + resp['linha-reta-distance'].toFixed(2) + "km </li>" +
-            "</ul> <br/>"
-        ;
-
+        var dashboard_string = createDashboardString(resp);
+        console.log(dashboard_string);
         $("div.main-dashboard").html(dashboard_string);
 
 
         /*
+        // colocar tb o tipo de gasosa, para paoder comparar com o co2.
         - graficozinhos relacionando cada uma das coisas visualizadas no mapa.
         Rpm
         Engine Load
         time inicio
         time fim
         --- procurar, tb, aqueles heatmaps.
-
 
         partes mais de inteligencia.
             - dada uma rota, quero saber quais os carros que passaram por ali.
