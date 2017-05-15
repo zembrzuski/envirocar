13438687c5bcbbcf6056d0e4d2a19914359d4422
rodrigo zembrzuski <rodrigo.zembrzuski@gruporbs.com.br>
2017-05-15 13:14:24 -0300

    caching reqs to google api

--- web_application/service/address_service/address_retrieval_google_maps.py
+++ web_application/service/address_service/address_retrieval_google_maps.py
@@ -1,3 +1,4 @@
+import redis
 import requests
 import json
 
@@ -9,8 +10,18 @@ def retrieve_address_by_coordinate(lat, lng):
     """
     Given a coordinate, retrieves the street information supposed to be.
     """
-    response = requests.get(URL.format(lat, lng, API_KEY))
-    the_json = json.loads(response.content.decode('utf-8'))
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    redis_key = str(lat) + "___" + str(lng)
+    cached_url = r.get(redis_key)
+
+    the_json = None
+
+    if cached_url:
+        the_json = json.loads(cached_url.decode('utf-8'))
+    else:
+        response = requests.get(URL.format(lat, lng, API_KEY))
+        the_json = json.loads(response.content.decode('utf-8'))
+        r.set(redis_key, json.dumps(the_json))
 
     addresses_components = list(map(lambda x: x['address_components'], the_json['results']))
 
