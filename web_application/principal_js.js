762afc0a4313d10f08607185a782991bb4d2cc82
Your Name <you@example.com>
2017-05-20 12:23:19 -0300

    starting to plot some charts with d3

--- web_application/principal_js.js
+++ web_application/principal_js.js
@@ -18,9 +18,9 @@ function initMap() {
 
         vel_max = resp['vel']['max'];
 
-        var dashboard_string = createDashboardString(resp);
-        console.log(dashboard_string);
-        $("div.main-dashboard").html(dashboard_string);
+        //var dashboard_string = createDashboardString(resp);
+        //console.log(dashboard_string);
+        //$("div.main-dashboard").html(dashboard_string);
 
         for (i = 0 ; i < coordinates.length-1; i++) {
           a = coordinates[i];
@@ -51,5 +51,98 @@ function initMap() {
 
           flightPath_1.setMap(map);
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        var data = [{
+            "xx": "202",
+            "year": "2000"
+        }, {
+            "xx": "215",
+            "year": "2001"
+        }, {
+            "xx": "179",
+            "year": "2002"
+        }, {
+            "xx": "199",
+            "year": "2003"
+        }, {
+            "xx": "134",
+            "year": "2003"
+        }, {
+            "xx": "176",
+            "year": "2010"
+        }];
+
+        var vis = d3.select("#visualisation"),
+            WIDTH = 500,
+            HEIGHT = 200,
+            MARGINS = {
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 50
+            },
+            xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([2000,2010]),
+            yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134,215]),
+            xAxis = d3.svg.axis().scale(xScale),
+            yAxis = d3.svg.axis().scale(yScale);
+
+            vis.append("svg:g")
+              .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+              .call(xAxis);
+
+            yAxis = d3.svg.axis()
+              .scale(yScale)
+              .orient("left");
+
+            vis.append("svg:g")
+              .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+              .call(yAxis);
+
+          var lineGen = d3.svg.line()
+              .x(function(d) {
+                return xScale(d.year);
+              })
+              .y(function(d) {
+                return yScale(d.xx);
+              })
+              .interpolate("basis");;
+
+
+            vis.append('svg:path')
+              .attr('d', lineGen(data))
+              .attr('stroke', 'green')
+              .attr('stroke-width', 2)
+              .attr('fill', 'none');
+
+          vis.append("svg:g")
+              .attr("class","axis")
+              .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+              .call(xAxis);
+           
+          vis.append("svg:g")
+              .attr("class","axis")
+              .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+              .call(yAxis);
+
+
     });
 }
