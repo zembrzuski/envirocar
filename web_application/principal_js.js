3e6291f07ea88b3778c88abfca51b28bb1e49d22
Rodrigo Zembrzuski <zembrzuski@gmail.com>
2017-07-03 16:51:22 -0300

    ae

--- web_application/principal_js.js
+++ web_application/principal_js.js
@@ -1,24 +1,25 @@
 var map;
 
-function initMap() {
-    var track_id = location.pathname.split("/map/")[1];
-    var flightPlanCoordinates;
+
+function plota(track_id, instantiate_map) {
 
     $.get('http://127.0.0.1:5000/find_track/' + track_id, function(data) {
         resp = JSON.parse(data);
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: resp['center'],
-          zoom: 13,
-          mapTypeId: 'terrain',
-          styles: map_styles
-        });
+
+        if (instantiate_map) {
+            map = new google.maps.Map(document.getElementById('map'), {
+              center: resp['center'],
+              zoom: 13,
+              mapTypeId: 'terrain',
+              styles: map_styles
+            });
+
+            return;
+        }
 
         coordinates = resp['coordinates'];
 
         vel_max = resp['vel']['max'];
-
-        console.log('vel_max: ' + vel_max);
-        console.log('vel_med: ' + resp['vm']);
 
         var dashboard_string = createDashboardString(resp);
         console.log(dashboard_string);
@@ -29,20 +30,9 @@ function initMap() {
           a = coordinates[i];
           b = coordinates[i+1];
 
-          //var maxspeed = parseFloat(coordinates[i]['trace']['maxspeed']);
           var vel = parseFloat(coordinates[i]['phenomenons']['Speed']['value']);
 
           kol = getColorForPercentage(1-vel/vel_max);
-
-//          kol = 'black';
-//          if (vel > maxspeed + maxspeed * 5/100) {
-//            kol = 'red';
-//          } else {
-//            kol = 'blue';
-//          }
-//          if (maxspeed == 0) {
-//            kol = 'yellow';
-//          }
 
           var flightPath_1 = new google.maps.Polyline({
             path: [a, b],
@@ -59,4 +49,10 @@ function initMap() {
         plot_speed_limit2(resp['consumption_scaled'], resp['rpm_scaled']);
 
     });
+}
+
+
+function initMap() {
+    var track_id = location.pathname.split("/map/")[1];
+    plota(track_id, true)
 }
